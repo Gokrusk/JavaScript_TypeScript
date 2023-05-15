9980fdd280a18ead76aeeee9ad4a7e2e28fe6efd
gokrusk <nigelljama@gmail.com>
2023-05-15 07:53:26 -0500

    math module

---
+++ math/index.js
@@ -0,0 +1,22 @@
+function add(x, y) {
+  return x + y;
+}
+
+function sub(x, y) {
+  return x * y;
+}
+
+function mult(x, y) {
+  return x * y;
+}
+
+function div(x, y) {
+  return x / y;
+}
+
+console.log(add(10, 20))
+console.log(sub(10,20))
+console.log(mult(10,20))
+console.log(div(10,20))
+
+module.exports.add = add
